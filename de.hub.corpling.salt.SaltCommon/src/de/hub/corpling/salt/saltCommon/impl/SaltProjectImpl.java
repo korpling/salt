4c50e15e0ecb92f58ad63b5d9a406111754320ac
FlorianZipser <f.zipser@gmx.de>
2010-08-19 12:41:42 +0000

    Ids for SCorpusGraph objects will be set automatically
    git-svn-id: https://korpling.german.hu-berlin.de/svn/saltnpepper/Salt/trunk@718 47085195-c50e-4317-8f43-7cb808db0f00

--- de.hub.corpling.salt.SaltCommon/src/de/hub/corpling/salt/saltCommon/impl/SaltProjectImpl.java
+++ de.hub.corpling.salt.SaltCommon/src/de/hub/corpling/salt/saltCommon/impl/SaltProjectImpl.java
@@ -6,25 +6,23 @@
  */
 package de.hub.corpling.salt.saltCommon.impl;
 
-import de.hub.corpling.salt.saltCommon.SaltCommonPackage;
-import de.hub.corpling.salt.saltCommon.SaltProject;
-
-import de.hub.corpling.salt.saltCommon.sCorpusStructure.SCorpusGraph;
-import de.hub.corpling.salt.saltCommon.sCorpusStructure.SCorpusStructurePackage;
-
 import java.util.Collection;
 
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
-
 import org.eclipse.emf.common.util.EList;
-
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.InternalEObject;
-
 import org.eclipse.emf.ecore.impl.EObjectImpl;
-
 import org.eclipse.emf.ecore.util.EObjectContainmentWithInverseEList;
 import org.eclipse.emf.ecore.util.InternalEList;
+
+import de.hub.corpling.salt.saltCommon.SaltCommonFactory;
+import de.hub.corpling.salt.saltCommon.SaltCommonPackage;
+import de.hub.corpling.salt.saltCommon.SaltProject;
+import de.hub.corpling.salt.saltCommon.sCorpusStructure.SCorpusGraph;
+import de.hub.corpling.salt.saltCommon.sCorpusStructure.SCorpusStructurePackage;
 
 /**
  * <!-- begin-user-doc -->
@@ -69,16 +67,48 @@ public class SaltProjectImpl extends EObjectImpl implements SaltProject {
 		return SaltCommonPackage.Literals.SALT_PROJECT;
 	}
 
+	private static final String KW_SCORPUS_GRAPH_ID="corpusGraph";
+	private int numOfSCorpusGraphs= 0;
+	@Override
+	public void eNotify(Notification notification) 
+	{
+		super.eNotify(notification);		
+		if (notification.getFeature() instanceof EReference) 
+		{
+			switch (notification.getEventType()) {
+			case Notification.ADD:
+				if (notification.getNewValue() instanceof SCorpusGraph)
+				{
+					SCorpusGraph sCorpusGraph= (SCorpusGraph)notification.getNewValue();
+					if (sCorpusGraph.getSElementId()== null)
+						sCorpusGraph.setSElementId(SaltCommonFactory.eINSTANCE.createSElementId());
+					if (	(sCorpusGraph.getSId()== null)||
+							(sCorpusGraph.getSId().equalsIgnoreCase("")))
+					{
+						this.numOfSCorpusGraphs++;
+						sCorpusGraph.setSId(KW_SCORPUS_GRAPH_ID + numOfSCorpusGraphs);
+					}
+				}
+			}
+		}
+	}
+	
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	public EList<SCorpusGraph> getSCorpusGraphs() {
+	public EList<SCorpusGraph> getSCorpusGraphs() 
+	{
 		if (sCorpusGraphs == null) {
 			sCorpusGraphs = new EObjectContainmentWithInverseEList<SCorpusGraph>(SCorpusGraph.class, this, SaltCommonPackage.SALT_PROJECT__SCORPUS_GRAPHS, SCorpusStructurePackage.SCORPUS_GRAPH__SALT_PROJECT);
 		}
 		return sCorpusGraphs;
+	}
+	
+	@Override
+	public boolean eNotificationRequired() {
+		return true;
 	}
 
 	/**
