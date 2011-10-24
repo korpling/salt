a5ec3c3aa14ae0e91a173a426e680709cfe4d7bc
FlorianZipser <f.zipser@gmx.de>
2011-10-24 09:38:24 +0000

    added more javadoc
    git-svn-id: https://korpling.german.hu-berlin.de/svn/saltnpepper/salt/trunk@2319 47085195-c50e-4317-8f43-7cb808db0f00

--- salt-graph/src/main/java/de/hu_berlin/german/korpling/saltnpepper/salt/graph/GraphTraverseHandler.java
+++ salt-graph/src/main/java/de/hu_berlin/german/korpling/saltnpepper/salt/graph/GraphTraverseHandler.java
@@ -1,15 +1,30 @@
 package de.hu_berlin.german.korpling.saltnpepper.salt.graph;
 
-
+/**
+ * The interface of a callback handler for traversing a {@link Graph} object. When invoking the method 
+ * {@link Graph#traverse(org.eclipse.emf.common.util.EList, GRAPH_TRAVERSE_TYPE, String, GraphTraverseHandler)} an object
+ * implementing this interface must be passed. This interface contains three methods {@link #checkConstraint(GRAPH_TRAVERSE_TYPE, String, Edge, Node, long)},
+ * {@link #nodeReached(GRAPH_TRAVERSE_TYPE, String, Node, Edge, Node, long)} and {@link #nodeLeft(GRAPH_TRAVERSE_TYPE, String, Node, Edge, Node, long)}.
+ * If the traversal reaches a new {@link Node} object being contained in the {@link Graph} object, the method
+ * {@link #checkConstraint(GRAPH_TRAVERSE_TYPE, String, Edge, Node, long)} will be invoked. If this method returns true, it means
+ * that this node and eventually its sub-graph will also be traversed. When true is returned, the method {@link #nodeReached(GRAPH_TRAVERSE_TYPE, String, Node, Edge, Node, long)}
+ * will be invoked, when reaching the node and {@link #nodeLeft(GRAPH_TRAVERSE_TYPE, String, Node, Edge, Node, long)} when lefting
+ * the node. 
+ * 
+ * @author Florian Zipser
+ *
+ */
 public interface GraphTraverseHandler {
 	/**
-	 * Call back method to notify the listener of traversing.
-	 * @param traversalMode
-	 * @param traversalId - a unique id for one traversing process, the same id as for GraphTraverserObject
-	 * @param currNode
-	 * @param edge
-	 * @param fromNode
-	 * @param order
+	 * This method will be invoked, when a node is reached, after the method {@link #checkConstraint(GRAPH_TRAVERSE_TYPE, String, Edge, Node, long)}
+	 * has returned true and before the method {@link #nodeLeft(GRAPH_TRAVERSE_TYPE, String, Node, Edge, Node, long)} 
+	 * is invoked.
+	 * @param traversalType the type of current traversal
+	 * @param traversalId the id of current traversal, given by the invoking object
+	 * @param currNode the current node
+	 * @param edge the edge via which the current node was reached
+	 * @param fromNode the parent node 
+	 * @param order the number of the edge in the parent node
 	 */
 	public void nodeReached(	GRAPH_TRAVERSE_TYPE traversalType, 
 								String traversalId,
@@ -19,12 +34,17 @@ public interface GraphTraverseHandler {
 								long order);
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @model
-	 * @generated
+	 * This method will be invoked, when a node is left, after the method {@link #checkConstraint(GRAPH_TRAVERSE_TYPE, String, Edge, Node, long)}
+	 * has returned true and the method {@link #nodeReached(GRAPH_TRAVERSE_TYPE, String, Node, Edge, Node, long)} 
+	 * has been invoked.
+	 * @param traversalType the type of current traversal
+	 * @param traversalId the id of current traversal, given by the invoking object
+	 * @param currNode the current node
+	 * @param edge the edge via which the current node was reached
+	 * @param fromNode the parent node 
+	 * @param order the number of the edge in the parent node
 	 */
-	public void nodeLeft(	GRAPH_TRAVERSE_TYPE traversalTypse, 
+	public void nodeLeft(	GRAPH_TRAVERSE_TYPE traversalType, 
 							String traversalId,
 							Node currNode, 
 							Edge edge, 
@@ -32,10 +52,18 @@ public interface GraphTraverseHandler {
 							long order);
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @model
-	 * @generated
+	 * This method is called during a traversal to check if the current node and eventually its sub-graph shall 
+	 * be traversed or not. If the return value is false, the traversion for the current path, will stop at the
+	 * current node and the methods {@link #nodeReached(GRAPH_TRAVERSE_TYPE, String, Node, Edge, Node, long)} and
+	 * {@link #nodeLeft(GRAPH_TRAVERSE_TYPE, String, Node, Edge, Node, long)} as well will not be invoked. Otherwise
+	 * the method {@link #nodeReached(GRAPH_TRAVERSE_TYPE, String, Node, Edge, Node, long)} will be invoked, when
+	 * a new node is reached and {@link #nodeLeft(GRAPH_TRAVERSE_TYPE, String, Node, Edge, Node, long)} will  be 
+	 * invoked, when this node is left.
+	 * @param traversalType the type of current traversal
+	 * @param traversalId the id of current traversal, given by the invoking object
+	 * @param edge the edge via which the current node was reached
+	 * @param currNode the current node
+	 * @param order the number of the edge in the parent node
 	 */
 	public boolean checkConstraint(		GRAPH_TRAVERSE_TYPE traversalType, 
 										String traversalId,
