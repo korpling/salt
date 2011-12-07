b6f676c3074b695ed6d74de12e7ef7b06d146bc8
eladrion <eladrion@cs.uni-potsdam.de>
2011-12-07 11:29:32 +0000

    Adding deprecation message vor javadoc
    git-svn-id: https://korpling.german.hu-berlin.de/svn/saltnpepper/salt/trunk@2559 47085195-c50e-4317-8f43-7cb808db0f00

--- salt-graph/src/main/java/de/hu_berlin/german/korpling/saltnpepper/salt/graph/modules/GraphTraverser.java
+++ salt-graph/src/main/java/de/hu_berlin/german/korpling/saltnpepper/salt/graph/modules/GraphTraverser.java
@@ -17,6 +17,7 @@
  */
 package de.hu_berlin.german.korpling.saltnpepper.salt.graph.modules;
 
+import java.awt.font.ShapeGraphicAttribute;
 import java.util.Collections;
 
 import org.eclipse.emf.common.util.BasicEList;
@@ -31,6 +32,7 @@ import de.hu_berlin.german.korpling.saltnpepper.salt.graph.exceptions.GraphExcep
 
 /**
  * This class offers some methods to traverse a given graph.
+ * Deprecated since salt 1.3. Please see SGraph#traverse
  * @author Florian Zipser
  *
  */
