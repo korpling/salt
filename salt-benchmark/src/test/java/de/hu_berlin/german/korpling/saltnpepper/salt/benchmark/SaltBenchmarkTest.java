510fec6632f0398248fd044f8a51951761f56f07
thomaskrause <krauseto@hu-berlin.de>
2013-06-05 11:47:03 +0000

    adding salt-benchmark project
    execute tests on this project to perform benchmark, the graphs are stored as html output file
    
    git-svn-id: https://korpling.german.hu-berlin.de/svn/saltnpepper/salt/branches/speed@8104 47085195-c50e-4317-8f43-7cb808db0f00

---
+++ salt-benchmark/src/test/java/de/hu_berlin/german/korpling/saltnpepper/salt/benchmark/SaltBenchmarkTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2013 Humboldt Univerity of Berlin, INRIA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.hu_berlin.german.korpling.saltnpepper.salt.benchmark;
+
+import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
+import com.carrotsearch.junitbenchmarks.BenchmarkRule;
+import com.carrotsearch.junitbenchmarks.annotation.BenchmarkHistoryChart;
+import com.carrotsearch.junitbenchmarks.annotation.BenchmarkMethodChart;
+import com.carrotsearch.junitbenchmarks.annotation.LabelType;
+import de.hu_berlin.german.korpling.saltnpepper.salt.graph.Graph;
+import de.hu_berlin.german.korpling.saltnpepper.salt.graph.GraphFactory;
+import de.hu_berlin.german.korpling.saltnpepper.salt.graph.Label;
+import de.hu_berlin.german.korpling.saltnpepper.salt.graph.Node;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+/**
+ *
+ * @author Thomas Krause <thomas.krause@alumni.hu-berlin.de>
+ */
+@BenchmarkOptions(callgc = false, benchmarkRounds = 10, warmupRounds = 5)
+@BenchmarkMethodChart(filePrefix = "benchmark-lists")
+@BenchmarkHistoryChart(labelWith = LabelType.TIMESTAMP, maxRuns = 20)
+public class SaltBenchmarkTest
+{
+  
+  @Rule
+  public TestRule benchmarkRun = new BenchmarkRule();
+  
+  public SaltBenchmarkTest()
+  {
+  }
+  
+  @BeforeClass
+  public static void setUpClass()
+  {
+  }
+  
+  @AfterClass
+  public static void tearDownClass()
+  {
+  }
+  
+  @Before
+  public void setUp()
+  {
+  }
+  
+  @After
+  public void tearDown()
+  {
+  }
+  
+   @Test
+   public void addNodesWithLabels() throws Exception {
+     
+     Graph g = GraphFactory.eINSTANCE.createGraph();
+        
+     for(int i=0; i < 20000; i++)
+     {
+        Node n =  GraphFactory.eINSTANCE.createNode();
+        
+        g.addNode(n);
+        
+        for(int l=0; l < 20; l++)
+        {
+          Label lbl = GraphFactory.eINSTANCE.createLabel();
+          lbl.setName("lbl" + l);
+          lbl.setNamespace("salt");
+          n.addLabel(lbl);
+        }
+        
+     }
+   }
+   
+}
