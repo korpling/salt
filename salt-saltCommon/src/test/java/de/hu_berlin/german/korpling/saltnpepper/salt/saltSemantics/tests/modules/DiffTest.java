f53604aa7a108f605b81531713f7d37bd53f1ae5
Florian Zipser <f.zipser@gmx.de>
2014-10-13 13:54:09 +0200

    added some tests

--- salt-saltCommon/src/test/java/de/hu_berlin/german/korpling/saltnpepper/salt/saltSemantics/tests/modules/DiffTest.java
+++ salt-saltCommon/src/test/java/de/hu_berlin/german/korpling/saltnpepper/salt/saltSemantics/tests/modules/DiffTest.java
@@ -24,7 +24,7 @@ import org.junit.Before;
 import de.hu_berlin.german.korpling.saltnpepper.salt.SaltFactory;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.modules.Diff;
 import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SDocumentGraph;
-import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SDocumentStructureFactory;
+import de.hu_berlin.german.korpling.saltnpepper.salt.saltCommon.sDocumentStructure.SToken;
 import de.hu_berlin.german.korpling.saltnpepper.salt.samples.SampleGenerator;
 
 /**
@@ -56,24 +56,104 @@ public class DiffTest extends TestCase {
 		
 		setFixture(new Diff(template, other));
 	}
-	/** Tests two graphs containing same primary data **/
-	public void testPrimaryData_same(){
+	/** Tests two graphs containing 
+	 * <ul>
+	 *  <li>same primary date</li>
+	 *  <li>different primary date</li>
+	 *  <li>two primary data</li>
+	 * </ul> 
+	 **/
+	public void testPrimaryData(){
 		SampleGenerator.createPrimaryData(template.getSDocument());
 		SampleGenerator.createPrimaryData(other.getSDocument());
 		
+		//same primary date
 		assertTrue(getFixture().findDiffs());
 		assertEquals(0, getFixture().getDifferences());
-	}
-	
-	/** Tests two graphs containing different primary data **/
-	public void testPrimaryData_different(){
-		SampleGenerator.createPrimaryData(template.getSDocument());
-		SampleGenerator.createPrimaryData(other.getSDocument());
-		other.getSTextualDSs().get(0).setSText("a different text");
 		
+		//different primary date
+		other= SaltFactory.eINSTANCE.createSDocumentGraph();
+		other.getSTextualDSs().get(0).setSText("a different text");
+		assertFalse(getFixture().findDiffs());
+		assertEquals(1, getFixture().getDifferences());
+		
+		//two primary date
+		other= SaltFactory.eINSTANCE.createSDocumentGraph();
+		SampleGenerator.createPrimaryData(other.getSDocument());
 		assertFalse(getFixture().findDiffs());
 		assertEquals(1, getFixture().getDifferences());
 	}
 	
-	
+	/**
+	 * Tests two graphs containing
+	 * <ul>
+	 * 	<li>same tokenization</li>
+	 *  <li>different tokenization - number of tokens</li>
+	 *  <li>different tokenization - different offsets</li>
+	 * </ul>
+	 */
+	public void testToken(){
+		SampleGenerator.createPrimaryData(template.getSDocument());
+		SampleGenerator.createTokens(template.getSDocument());
+		SampleGenerator.createPrimaryData(other.getSDocument());
+		SampleGenerator.createTokens(other.getSDocument());
+		
+		//same tokenization
+		assertTrue(getFixture().findDiffs());
+		assertEquals(0, getFixture().getDifferences());
+		
+		//different tokenization - number of tokens
+		other.createSToken(other.getSTextualDSs().get(0), 1, 3);
+		assertFalse(getFixture().findDiffs());
+		assertEquals(1, getFixture().getDifferences());
+		
+		//different tokenization - different offsets 
+		other= SaltFactory.eINSTANCE.createSDocumentGraph();
+		SampleGenerator.createPrimaryData(other.getSDocument());
+		SampleGenerator.createTokens(other.getSDocument());
+		other.removeNode(other.getSTokens().get(other.getSTokens().size()-1));
+		other.createSToken(other.getSTextualDSs().get(0), 1, 20);
+		assertFalse(getFixture().findDiffs());
+		assertEquals(1, getFixture().getDifferences());
+	}
+	/**
+	 * Checks the isomorphie for token annotation for listed cases:
+	 * <ul>
+	 *  <li>same annotations</li>
+	 *  <li>different annotations - one additional annotation</li>
+	 *  <li>different annotations - different annotation names</li>
+	 * </ul>
+	 */
+	public void testTokenAnnotation(){
+		SampleGenerator.createPrimaryData(template.getSDocument());
+		SampleGenerator.createTokens(template.getSDocument());
+		SampleGenerator.createPrimaryData(other.getSDocument());
+		SampleGenerator.createTokens(other.getSDocument());
+		
+		for (SToken tok: template.getSTokens()){
+			tok.createSAnnotation(null, "annoName", "annoVal");
+		}
+		
+		for (SToken tok: other.getSTokens()){
+			tok.createSAnnotation(null, "annoName", "annoVal");
+		}
+		//same annotations
+		assertTrue(getFixture().findDiffs());
+		assertEquals(0, getFixture().getDifferences());
+		
+		//different annotations - one additional annotation
+		other.getSTokens().get(0).createSAnnotation(null, "annoName2", "annoVal");
+		assertFalse(getFixture().findDiffs());
+		assertEquals(1, getFixture().getDifferences());
+		
+		//different annotations - different annotation names
+		other= SaltFactory.eINSTANCE.createSDocumentGraph();
+		SampleGenerator.createPrimaryData(other.getSDocument());
+		SampleGenerator.createTokens(other.getSDocument());
+		for (SToken tok: other.getSTokens()){
+			tok.createSAnnotation(null, "otherName", "annoVal");
+		}
+		assertFalse(getFixture().findDiffs());
+		assertEquals(other.getSTokens().size(), getFixture().getDifferences());
+	}
 }
