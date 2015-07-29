f6138b34ae3f1e0702872c9f5b5736ec4344c6b0
f.zipser@gmx.de <f.zipser@gmx.de>
2015-07-29 18:29:40 +0200

    renamed NamedElement into SNamedElement and added a Javadoc to the provided methods.

--- salt-core/src/main/java/de/hu_berlin/u/saltnpepper/salt/core/SLayer.java
+++ salt-core/src/main/java/de/hu_berlin/u/saltnpepper/salt/core/SLayer.java
@@ -2,6 +2,6 @@ package de.hu_berlin.u.saltnpepper.salt.core;
 
 import de.hu_berlin.u.saltnpepper.graph.Layer;
 
-public interface SLayer extends Layer<SNode, SRelation<SNode,SNode>>, SAnnotationContainer, NamedElement {
+public interface SLayer extends Layer<SNode, SRelation<SNode,SNode>>, SAnnotationContainer, SNamedElement {
 
 }
