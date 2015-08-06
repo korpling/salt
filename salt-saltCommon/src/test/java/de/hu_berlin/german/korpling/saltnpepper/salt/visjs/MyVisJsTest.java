e5c8c04a731493a01aa4a1919120cc0ee99b8d27
irina <irina.glushanok@gmail.com>
2015-08-06 19:21:56 +0200

    added xml output

--- salt-saltCommon/src/test/java/de/hu_berlin/german/korpling/saltnpepper/salt/visjs/MyVisJsTest.java
+++ salt-saltCommon/src/test/java/de/hu_berlin/german/korpling/saltnpepper/salt/visjs/MyVisJsTest.java
@@ -67,20 +67,64 @@ public class MyVisJsTest implements SGraphTraverseHandler{
 	private static final String TRAV_MODE_CALC_LEVEL = "calcLevel";
 	private static final String TRAV_MODE_READ_NODES = "readNodes";
 	
+	
 	private HashSet <SNode> readRoots;
 	private EList<SNode> roots;
+	private int nGroupsId = 0;
 	
+	
+	// JSON output
 	private String id = "id";
 	private String label = "label";
 	private String color = "color";
 	private String x = "x";
 	private String level = "level";
+	private String group = "group";
 	
 	private String from = "{from: ";
 	private String to = "to: ";
 	
 	private int xValue = 0;	
 	private String tokColorValue = "#CCFF99";
+	//private int groupValue;
+	
+	
+	// HTML output
+	private  final String visJsSrc = "../../../dist/vis.js";
+	private final String visCss = "../../../dist/vis.css";
+	private final String visJsSrcGA = "../../googleAnalytics.js";
+	
+    private final String textStyle = "width:700px; font-size:14px; text-align: justify;";
+    
+    //HTML tags
+    private final String html = "html";
+    private final String head = "head";
+    private final String body = "body";
+    private final String title = "title";
+    private final String p = "p";
+    private final String div = "div";
+    private final String script = "script";
+    private final String style = "style";
+    private final String link = "link";
+    private final String h2 = "h2";
+    private final String input = "input";
+    
+    //HTML attributes
+    private final String attType = "type";
+    private final String attId = "id";
+    private final String attValue = "value";
+    private final String attSrc = "src";
+    private final String attHref = "href";
+    private final String attRel = "rel";
+    private final String attStyle = "style";
+    private final String attLang = "language";
+    
+    
+    
+    
+    private final String newline = "\n";
+    
+ 
 	
 	
 	
@@ -89,15 +133,31 @@ public MyVisJsTest (){
 	 this.doc= SaltFactory.eINSTANCE.createSDocument();
 	SampleGenerator.createSDocumentStructure(doc);
 	roots = doc.getSDocumentGraph().getSRoots();
+	
+	EList<SSpan>  sSpans = doc.getSDocumentGraph().getSSpans();
+	
+	
+	if (sSpans != null && (sSpans.size() > 0)){
+		nGroupsId += 1;
+	}
+	
+	EList<SStructure>  sStructures = doc.getSDocumentGraph().getSStructures();
+	
+	if (sStructures != null && (sStructures.size() > 0)){
+		nGroupsId += 2;
+	}
+	
 	// this.os = new FileOutputStream(new File ("outputHTMLTest.html"));	
 	readRoots = new HashSet <SNode>();
 	this.outputFactory = XMLOutputFactory.newInstance();
 	try {
-		this.writer = outputFactory.createXMLStreamWriter(System.out);
+		this.writer = outputFactory.createXMLStreamWriter(System.out, "UTF-8");
 	} catch (XMLStreamException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
+	
+	
 	this.out = new BufferedWriter(new OutputStreamWriter(System.out));		
 	this.jsonWriter = new JSONWriter(out);
 	this.strBuilder = new StringBuilder();
@@ -116,54 +176,209 @@ public MyVisJsTest (){
 
 
 		
-	/*	writer.writeStartDocument();
-		writer.setPrefix("c","http://c");
-		writer.setDefaultNamespace("http://c");
-		writer.writeCharacters("\n");     
+		writer.writeStartDocument();
+		writer.writeCharacters(newline);
+		writer.writeStartElement(html);
+		writer.writeCharacters(newline);
 		
-        writer.writeCharacters("\n");
-		writer.writeStartElement("http://c","a");
-		writer.writeAttribute("b","blah");
-		writer.writeNamespace("c","http://c");
-		writer.writeDefaultNamespace("http://c");
-		writer.setPrefix("d","http://c");
-		writer.writeCharacters("\n");
-		writer.writeEmptyElement("http://c","d");
-		writer.writeAttribute("http://c","chris","fry");
-		writer.writeNamespace("d","http://c");
-		writer.writeCharacters("Jean Arp");
+		writer.writeStartElement(head);
+		writer.writeCharacters(newline);
+		
+		writer.writeStartElement(title);
+		writer.writeCharacters("Network | Hierarchical Layout, userDefined");
 		writer.writeEndElement();
+		writer.writeCharacters(newline);
+		
+		writer.writeStartElement(style);
+		writer.writeAttribute(attType, "text/css");
+		writer.writeCharacters("body {\nfont: 10pt sans;\n}\n#mynetwork {\nwidth: 1000px;\nheight: 1000px;\nborder: 1px solid lightgray;\n}");
+		writer.writeEndElement();
+		writer.writeCharacters(newline);
+		
+		writer.writeStartElement(script);
+		writer.writeAttribute(attType, "text/javascript");
+		writer.writeAttribute(attSrc,visJsSrc);
+		writer.writeEndElement();
+		writer.writeCharacters(newline);
+		
+		writer.writeEmptyElement(link);
+		writer.writeAttribute(attHref, visCss);
+		writer.writeAttribute(attRel,"stylesheet");
+		writer.writeAttribute(attType, "text/css");
+		writer.writeCharacters(newline);
+		
+		
+		writer.writeStartElement(script);
+		writer.writeAttribute(attType, "text/javascript");
+		writer.writeCharacters("var nodes = null;\nvar edges = null;\nvar network = null;\n"
+						+ "var directionInput = document.getElementById(\"direction\");\n" 
+						+ "function destroy() {\n" 
+						+ "if (network !== null) {\n" 
+						+ "network.destroy();\n" 
+						+ "network = null;\n" 
+						+ "}\n" 
+						+ "}\n" 
+						+ "function draw() {\n" 
+						+ "destroy();\n" 
+						+ "var connectionCount = [];\n" 
+						+ "nodes = [];\n" 
+						+ "edges = [];);\n");
+		
+		writer.flush();
+		
+		  writeJSON();
+		
+		writer.writeCharacters("var container = document.getElementById('mynetwork');\n +"
+				+ "var data = {\n "
+				+ "nodes: nodes,\n"
+				+ "edges: edges\n"
+				+ "};\n"
+				+ "var options = {\n"
+				+ "nodes:{\n"
+				+ "shadow: true,\n"
+				+ "shape: \"box\"\n"
+				+ "},\n"
+				+ "edges: {\n"
+				+ "smooth: true\n"
+				+ "},\n"
+				+ "layout: {\n"
+				+ "hierarchical:{\n"
+				+ "direction: directionInput.value\n"
+				+ "}\n"
+				+ "}\n"
+				+ "};\n"
+				+ "network = new vis.Network(container, data, options);\n"
+				+ "network.on('select', function(params) {\n"
+				+ "document.getElementById('selection').innerHTML = 'Selection: ' + params.nodes;\n"
+				+ "});\n"
+				+ "}\n");
+
+		writer.writeEndElement();
+		writer.writeCharacters(newline);
+		
+		writer.writeStartElement(script);
+		writer.writeAttribute(attSrc, visJsSrcGA);
+		writer.writeEndElement();
+		writer.writeCharacters(newline);		
+		
+		//head 
+		writer.writeEndElement();
+		writer.writeCharacters(newline);
+		
+		
+		writer.writeStartElement(body);
+		writer.writeAttribute("onload", "draw();");
+		writer.writeCharacters(newline);
+		
+		writer.writeStartElement(h2);
+		writer.writeCharacters("Hierarchical Layout");
+		writer.writeEndElement();
+		writer.writeCharacters(newline);
+		
+		writer.writeStartElement(div);
+		writer.writeAttribute(attStyle,textStyle);
+		writer.writeCharacters("Ein Beispiel");
+		writer.writeEndElement();
+		writer.writeCharacters(newline);
+		
+		writer.writeStartElement("p");
+		
+		writer.writeEmptyElement(input);
+		writer.writeAttribute(attType,"button");
+		writer.writeAttribute(attId, "btn-UD");
+		writer.writeAttribute(attValue, "Up-Down");
+		writer.writeCharacters(newline);
+		
+		writer.writeEmptyElement(input);
+		writer.writeAttribute(attType,"button");
+		writer.writeAttribute(attId, "btn-DU");
+		writer.writeAttribute(attValue, "Down-Up");
+		writer.writeCharacters(newline);
+		
+		writer.writeEmptyElement(input);
+		writer.writeAttribute(attType,"button");
+		writer.writeAttribute(attId, "btn-LR");
+		writer.writeAttribute("value", "Left-Right");
+		writer.writeCharacters(newline);
+		
+		writer.writeEmptyElement(input);
+		writer.writeAttribute(attType,"button");
+		writer.writeAttribute(attId, "btn-RL");
+		writer.writeAttribute(attValue, "Right-Left");
+		writer.writeCharacters(newline);
+		
+		writer.writeEmptyElement(input);
+		writer.writeAttribute(attType,"hidden");
+		//TODO check the apostrophes
+		writer.writeAttribute(attId, "'direction'");
+		writer.writeAttribute(attValue, "UD");
+		writer.writeCharacters(newline);
+	
+		
+		//p
+		writer.writeEndElement();
+		writer.writeCharacters(newline);
+		
+		writer.writeStartElement(div);
+		writer.writeAttribute(attId, "mynetwork");
+		writer.writeEndElement();
+		writer.writeCharacters(newline);
+		
+		writer.writeStartElement(p);
+		writer.writeAttribute(attId, "selection");
+		writer.writeEndElement();
+		writer.writeCharacters(newline);
+		
+		
+		writer.writeStartElement(script);
+		writer.writeAttribute(attLang, "JavaScript");
+		writer.writeCharacters("var directionInput = document.getElementById(\"direction\");\n"
+				+ "var btnUD = document.getElementById(\"btn-UD\");\n"
+				+ "btnUD.onclick = function() {\n"
+				+ "directionInput.value = \"UD\";\n"
+				+ "draw();\n"
+				+ "};\n"
+				+ "var btnDU = document.getElementById(\"btn-DU\");\n"
+				+ "btnDU.onclick = function() {\n"
+				+ "directionInput.value = \"DU\";\n"
+				+ "draw();\n"
+				+ "};\n"
+				+ "var btnLR = document.getElementById(\"btn-LR\");\n"
+				+ "btnLR.onclick = function() {\n"
+				+ "directionInput.value = \"LR\";\n"
+				+ "draw();\n"
+				+ "};\n"
+				+ "var btnRL = document.getElementById(\"btn-RL\");\n"
+				+ "btnRL.onclick = function() {\n"
+				+ "directionInput.value = \"RL\";\n"
+				+ "draw();\n"
+				+ "};\n");
+		writer.writeEndElement();
+		writer.writeCharacters(newline);	
+		
+		
+		//body
+		writer.writeEndElement();
+		writer.writeCharacters(newline);
+		
+		
+		
+		
+		//html
+		writer.writeEndElement();
+		writer.writeCharacters(newline);	
+		
+		
+		
+	   
+		writer.writeEndDocument();
 		writer.flush();
 		writer.close();
 		
-		out.close();*/
+		out.close();
 		
 		
-	/*   EList <SToken> sTokens = doc.getSDocumentGraph().getSTokens();	   
-	   doc.getSDocumentGraph().sortSTokenByText();	  
-	   
-	  for(SToken sToken : sTokens ){
-		   System.out.println("Token: " + sToken );
-		   EList<SAnnotation> annotations = sToken.getSAnnotations();
-		   for (SAnnotation ann : annotations) {
-			   System.out.println(ann.getSName() + ":  " + ann.getSValue());
-		   }
-	   }
-	   System.out.println("\n");
 	  
-	   EList <SNode> sNodes = doc.getSDocumentGraph().getSNodes();
-	
-	   for(SNode sNode : sNodes ){
-		   System.out.println("Node: " +sNode +"; " + doc.getSDocumentGraph().getSText(sNode));
-		  
-	   }*/	
-	  
-	  
-	
-	  writeJSON();
-	   
-	  
-	   
 	//   System.out.println("currh: " + currHigh + " maxh: " + maxHigh);
 	  
 	}
@@ -194,7 +409,8 @@ public MyVisJsTest (){
 		
 	
 		 
-		 doc.getSDocumentGraph().traverse(doc.getSDocumentGraph().getSRoots(), GRAPH_TRAVERSE_TYPE.TOP_DOWN_DEPTH_FIRST, TRAV_MODE_READ_NODES, this);		
+		 doc.getSDocumentGraph().traverse(doc.getSDocumentGraph().getSRoots(), GRAPH_TRAVERSE_TYPE.TOP_DOWN_DEPTH_FIRST, 
+				 												TRAV_MODE_READ_NODES, this);		
 		 		 
 		 jsonWriter.endArray();			   
 		 out.append(");\n");
@@ -207,7 +423,7 @@ public MyVisJsTest (){
 		 strBuilder.append(");\n");
 		 
 		 out.append(strBuilder.toString());
-		 out.append("\n");
+		 out.append(newline);
 		 out.flush();
 		
 
@@ -237,7 +453,7 @@ public MyVisJsTest (){
 		   EList<SAnnotation> sAnnotations = node.getSAnnotations();
 		   if (sAnnotations.size() > 0){
 			   for (SAnnotation annotation : sAnnotations) {
-				   allLabels += (annotation.getSName() + "=" + annotation.getSValue().toString() + "\n");								   
+				   allLabels += (annotation.getSName() + "=" + annotation.getSValue().toString() + newline);								   
 			   }
 		   }
 		   
@@ -258,11 +474,32 @@ public MyVisJsTest (){
 		 
 		 	 jsonWriter.key(level);
 			 jsonWriter.value(levelValue);
+			 
+		if (node instanceof SSpan){
+			if (nGroupsId == 3){
+				 jsonWriter.key(group);
+				 jsonWriter.value("1");
+			}
+			else if (nGroupsId == 1){
+				jsonWriter.key(group);
+				jsonWriter.value("0");
+			}
+			// TODO error handling
+			
+		}
+		
+		if (node instanceof SStructure){
+			if (nGroupsId == 3 || nGroupsId == 2){
+				jsonWriter.key(group);
+				jsonWriter.value("0");
+			}
+			// TODO error handling
+		}
 		 
 		 
 		 jsonWriter.endObject();		
 
-		out.append("\n");
+		out.append(newline);
 	} catch (IOException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
@@ -352,7 +589,7 @@ public MyVisJsTest (){
 				  strBuilder.append(to);
 				  strBuilder.append("\"" +currNode.getSElementPath().fragment() + "\"}");
 				  strBuilder.append(",");
-				  strBuilder.append("\n");
+				  strBuilder.append(newline);
 				
 				  
 				}
