679dafefd4df44b59e8101c460295524deeb7faa
f.zipser@gmx.de <f.zipser@gmx.de>
2015-08-27 15:48:19 +0200

    added more tests for SMediaRelation, STextualRelation and STimelineRelation

---
+++ salt-core/src/test/java/de/hu_berlin/u/saltnpepper/salt/saltCommon/sDocumentStructure/tests/SSequentialRelationTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2009 Humboldt-Universität zu Berlin, INRIA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+package de.hu_berlin.u.saltnpepper.salt.saltCommon.sDocumentStructure.tests;
+
+import org.junit.Test;
+
+import de.hu_berlin.u.saltnpepper.salt.common.documentStructure.SSequentialRelation;
+import de.hu_berlin.u.saltnpepper.salt.core.SNode;
+
+public abstract class SSequentialRelationTest<S extends SNode, T extends SNode, P> {
+
+	protected SSequentialRelation<S, T, P> fixture = null;
+
+	protected void setFixture(SSequentialRelation<S, T, P> fixture) {
+		this.fixture = fixture;
+	}
+
+	protected SSequentialRelation<S, T, P> getFixture() {
+		return fixture;
+	}
+
+	/** Tests whether the start value is set and returned correct. **/
+	@Test
+	public abstract void testSetGetStart();
+
+	/** Tests whether the end value is set and returned correct. **/
+	@Test
+	public abstract void testSetGetEnd();
+} // SSequentialDSTest
