8432f897731596abd3599306024156e8a74b0602
f.zipser@gmx.de <f.zipser@gmx.de>
2016-01-30 14:48:44 +0100

    removed old salt-core project

--- salt-core/src/main/java/de/hu_berlin/u/saltnpepper/salt/core/SLayer.java
+++
@@ -1,7 +0,0 @@
-package de.hu_berlin.u.saltnpepper.salt.core;
-
-import de.hu_berlin.u.saltnpepper.graph.Layer;
-
-public interface SLayer extends Layer<SNode, SRelation<SNode, SNode>>, SAnnotationContainer, SNamedElement {
-
-}
