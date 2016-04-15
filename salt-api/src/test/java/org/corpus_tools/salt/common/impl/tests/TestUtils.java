10f29555f9a6474e6bf6fee024b9541adcad741b
Thomas Krause <thomaskrause@posteo.de>
2016-04-15 20:03:04 +0200

    the new function throws a different exception

--- salt-api/src/test/java/org/corpus_tools/salt/common/impl/tests/TestUtils.java
+++ salt-api/src/test/java/org/corpus_tools/salt/common/impl/tests/TestUtils.java
@@ -24,6 +24,7 @@ import org.corpus_tools.salt.SaltFactory;
 import org.corpus_tools.salt.common.SDocumentGraph;
 import org.corpus_tools.salt.core.SNode;
 import org.corpus_tools.salt.core.SRelation;
+import org.corpus_tools.salt.exceptions.SaltInsertionException;
 import org.corpus_tools.salt.exceptions.SaltParameterException;
 
 public class TestUtils {
@@ -44,7 +45,7 @@ public class TestUtils {
 		try {
 			SaltFactory.createSCorpusGraph().addRelation(fixture);
 			fail();
-		} catch (SaltParameterException e) {
+		} catch (SaltInsertionException e) {
 		}
 		SDocumentGraph graph = SaltFactory.createSDocumentGraph();
 		if (fixture.getSource() != null) {
