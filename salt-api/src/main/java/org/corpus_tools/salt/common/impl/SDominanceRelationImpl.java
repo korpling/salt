aec232a2108d82abbb03713c93013e9e7bd63d12
Thomas Krause <thomaskrause@posteo.de>
2016-04-18 13:38:22 +0200

    remove unecessary generic function parameter

--- salt-api/src/main/java/org/corpus_tools/salt/common/impl/SDominanceRelationImpl.java
+++ salt-api/src/main/java/org/corpus_tools/salt/common/impl/SDominanceRelationImpl.java
@@ -21,7 +21,6 @@ import org.corpus_tools.salt.common.SDocumentGraph;
 import org.corpus_tools.salt.common.SDominanceRelation;
 import org.corpus_tools.salt.common.SStructure;
 import org.corpus_tools.salt.common.SStructuredNode;
-import org.corpus_tools.salt.core.SGraph;
 import org.corpus_tools.salt.core.impl.SRelationImpl;
 import org.corpus_tools.salt.exceptions.SaltParameterException;
 import org.corpus_tools.salt.graph.Graph;
