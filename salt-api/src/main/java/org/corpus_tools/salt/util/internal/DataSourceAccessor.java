4302341b25cef088caddde7a80de671f86d2eda9
florian <florian@faramir>
2016-11-25 23:22:48 +0100

    fixes #110

--- salt-api/src/main/java/org/corpus_tools/salt/util/internal/DataSourceAccessor.java
+++ salt-api/src/main/java/org/corpus_tools/salt/util/internal/DataSourceAccessor.java
@@ -43,6 +43,7 @@ import org.corpus_tools.salt.core.SNode;
 import org.corpus_tools.salt.core.SRelation;
 import org.corpus_tools.salt.exceptions.SaltInvalidModelException;
 import org.corpus_tools.salt.exceptions.SaltParameterException;
+import org.corpus_tools.salt.graph.Relation;
 import org.corpus_tools.salt.util.DataSourceSequence;
 import org.corpus_tools.salt.util.STextualRelationSStartComparator;
 import org.corpus_tools.salt.util.SaltUtil;
@@ -176,13 +177,28 @@ public class DataSourceAccessor {
 			throw new SaltParameterException("Cannot start method please set the document graph first.");
 		}
 
-		final List<? extends SSequentialRelation> relations = findRelationsToDataSourceType(documentGraph, sequence);
+		final List<? extends SSequentialRelation> relations = findRelationsToDataSourceSequence(documentGraph,
+				sequence);
 		final List<SToken> tokens = findTokensReferedByRelationsInSequence(relations, sequence);
-		final List<SNode> nodes = new ArrayList<SNode>(tokens);
+		final List<SNode> nodes = new ArrayList<>();
+		nodes.addAll(findNodesOverlappingTokens(documentGraph, tokens, nodeClasses,
+				findRelationTypeToDataSourceSequence(sequence)));
 		return (nodes);
 	}
 
-	private static List<? extends SSequentialRelation> findRelationsToDataSourceType(SDocumentGraph documentGraph,
+	private static Class<? extends Relation> findRelationTypeToDataSourceSequence(DataSourceSequence<?> sequence) {
+		if (sequence == null || sequence.getDataSource() == null) {
+			return null;
+		}
+		if (sequence.getDataSource() instanceof STextualDS) {
+			return STextOverlappingRelation.class;
+		} else if (sequence.getDataSource() instanceof STimeline) {
+			return STimeOverlappingRelation.class;
+		}
+		return null;
+	}
+
+	private static List<? extends SSequentialRelation> findRelationsToDataSourceSequence(SDocumentGraph documentGraph,
 			DataSourceSequence<Number> sequence) {
 		List<? extends SSequentialRelation> relations = null;
 		if (sequence.getDataSource() instanceof STextualDS) {
@@ -209,6 +225,49 @@ public class DataSourceAccessor {
 			}
 		}
 		return tokens;
+	}
+
+	private static Set<SNode> findNodesOverlappingTokens(SDocumentGraph documentGraph, List<SToken> tokens,
+			final List<Class<? extends SNode>> nodeClasses, final Class<? extends Relation> relationClazz) {
+		if (tokens.isEmpty()) {
+			return Collections.emptySet();
+		}
+		final Set<SNode> overlappingNodes = new HashSet<>();
+		documentGraph.traverse((List<SNode>) (List<? extends SNode>) tokens, GRAPH_TRAVERSE_TYPE.BOTTOM_UP_DEPTH_FIRST,
+				"", new GraphTraverseHandler() {
+					@Override
+					public void nodeReached(GRAPH_TRAVERSE_TYPE traversalType, String traversalId, SNode currNode,
+							SRelation<SNode, SNode> relation, SNode fromNode, long order) {
+						if (nodeClasses == null) {
+							return;
+						}
+						for (Class<? extends SNode> nodeClass : nodeClasses) {
+							if (nodeClass.isAssignableFrom(currNode.getClass())) {
+								overlappingNodes.add(currNode);
+							}
+						}
+					}
+
+					@Override
+					public void nodeLeft(GRAPH_TRAVERSE_TYPE traversalType, String traversalId, SNode currNode,
+							SRelation<SNode, SNode> relation, SNode fromNode, long order) {
+					}
+
+					@Override
+					public boolean checkConstraint(GRAPH_TRAVERSE_TYPE traversalType, String traversalId,
+							SRelation<SNode, SNode> relation, SNode currNode, long order) {
+						if (relation == null) {
+							return true;
+						}
+						if (relation != null && relationClazz != null) {
+							if (relationClazz.isAssignableFrom(relation.getClass())) {
+								return true;
+							}
+						}
+						return false;
+					}
+				}, false);
+		return overlappingNodes;
 	}
 
 	/**
