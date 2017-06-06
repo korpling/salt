42a9d8001166793aaa5dda064ef8c25092bbac04
florian <f.zipser@gmx.de>
2017-06-06 22:56:35 +0200

    moved bottomupbradthfirst and topdownbreadthfirst traversal to its own class

---
+++ salt-api/src/main/java/org/corpus_tools/salt/core/impl/BottomUpBreadthFirstTraverser.java
@@ -0,0 +1,90 @@
+package org.corpus_tools.salt.core.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.corpus_tools.salt.core.GraphTraverseHandler;
+import org.corpus_tools.salt.core.SGraph;
+import org.corpus_tools.salt.core.SGraph.GRAPH_TRAVERSE_TYPE;
+import org.corpus_tools.salt.core.SNode;
+import org.corpus_tools.salt.core.SRelation;
+import org.corpus_tools.salt.exceptions.SaltException;
+import org.corpus_tools.salt.exceptions.SaltParameterException;
+
+public class BottomUpBreadthFirstTraverser extends Traverser {
+
+	public BottomUpBreadthFirstTraverser(List<? extends SNode> startNodes, GRAPH_TRAVERSE_TYPE strategy,
+			String traverseId, GraphTraverseHandler traverseHandler, boolean isCycleSafe, SGraph graph) {
+		super(startNodes, strategy, traverseId, traverseHandler, isCycleSafe, graph);
+	}
+
+	@Override
+	public void traverse() {
+		// BOTTOM_UP_BREADTH_FIRST traversal
+		for (SNode startNode : startNodes) {
+			currentNodePath = new ArrayList<SNode>();
+			if (traverseHandler.checkConstraint(strategy, traverseId, null, startNode, 0l)) {
+				currentNodePath.add(startNode);
+			}
+		}
+		if (isCycleSafe) {
+			throw new SaltException("Not able to detect cycles with breadth first search");
+		}
+		if ((currentNodePath == null) || (currentNodePath.isEmpty())) {
+			throw new SaltParameterException("Cannot traverse node starting at empty start node.");
+		}
+		final boolean isTopDown = strategy != GRAPH_TRAVERSE_TYPE.BOTTOM_UP_BREADTH_FIRST;
+		SNode fromNode = null;
+		SRelation<?, ?> fromRel = null;
+		List<SNode> queuedNodes = new ArrayList<>();
+		List<SNode> queueReachedFrom = new ArrayList<>();
+		List<SRelation<? extends SNode, ? extends SNode>> queueReachedFromRel = new ArrayList<>();
+		List<Integer> queueReachedOrder = new ArrayList<>();
+
+		queuedNodes.addAll(startNodes);
+		for (SNode startNode : startNodes) {
+			queueReachedFrom.add(startNode);
+			queueReachedFromRel.add(null);
+			queueReachedOrder.add(0);
+		}
+		currentNodePath.clear();
+		while (!queuedNodes.isEmpty()) {
+			SNode tNode = queuedNodes.remove(0);
+			fromNode = queueReachedFrom.remove(0);
+			fromRel = queueReachedFromRel.remove(0);
+			Integer order = queueReachedOrder.remove(0);
+
+			List<SRelation<? extends SNode, ? extends SNode>> edgesIn = graph.getInRelations(tNode.getId());
+			List<SRelation<? extends SNode, ? extends SNode>> edgesOut = graph.getOutRelations(tNode.getId());
+			List<SRelation<? extends SNode, ? extends SNode>> edges = null;
+			currentNodePath.add(tNode);
+			traverseHandler.nodeReached(strategy, traverseId, tNode, fromRel, fromNode, order);
+			if (isTopDown) {
+				// Switching in and out nodes list
+				edges = edgesOut;
+			} else {
+				edges = edgesIn;
+				// edgesIn = edgesOut;
+				// edgesOut = edges;
+			}
+			if (edges != null) {
+				// in case of node has childs
+				int orderCount = 0;
+				for (SRelation<? extends SNode, ? extends SNode> e : edges) {
+					SNode n = null;
+					n = (isTopDown) ? e.getTarget() : e.getSource();
+
+					if (traverseHandler.checkConstraint(strategy, traverseId, e, n, order)) {
+						queuedNodes.add(n);
+						queueReachedFrom.add(fromNode);
+						queueReachedFromRel.add(e);
+						queueReachedOrder.add(orderCount);
+					}
+					++orderCount;
+				}
+			}
+			traverseHandler.nodeLeft(strategy, traverseId, tNode, fromRel, fromNode, order);
+			currentNodePath.remove(0);
+		}
+	}
+}
--- salt-api/src/main/java/org/corpus_tools/salt/core/impl/GraphTraverserModule.java
+++ salt-api/src/main/java/org/corpus_tools/salt/core/impl/GraphTraverserModule.java
@@ -22,31 +22,20 @@ import static org.corpus_tools.salt.core.SGraph.GRAPH_TRAVERSE_TYPE.BOTTOM_UP_DE
 import static org.corpus_tools.salt.core.SGraph.GRAPH_TRAVERSE_TYPE.TOP_DOWN_BREADTH_FIRST;
 import static org.corpus_tools.salt.core.SGraph.GRAPH_TRAVERSE_TYPE.TOP_DOWN_DEPTH_FIRST;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.corpus_tools.salt.core.GraphTraverseHandler;
 import org.corpus_tools.salt.core.SGraph;
 import org.corpus_tools.salt.core.SGraph.GRAPH_TRAVERSE_TYPE;
 import org.corpus_tools.salt.core.SNode;
 import org.corpus_tools.salt.core.SRelation;
-import org.corpus_tools.salt.exceptions.SaltException;
 import org.corpus_tools.salt.exceptions.SaltInvalidModelException;
-import org.corpus_tools.salt.exceptions.SaltParameterException;
 import org.corpus_tools.salt.exceptions.SaltTraverserException;
 
 /**
  * Handles the traversal of a graph.
  */
 public class GraphTraverserModule {
-	/**
-	 * Stores all used traverseIds to the corresponding traverse callback
-	 * handler. This is used, to check, that an id is not used twice.
-	 */
-	private Map<GraphTraverseHandler, List<String>> traverseIdTable = new HashMap<>();
-
 	protected SGraph graph = null;
 
 	public SGraph getGraph() {
@@ -141,23 +130,6 @@ public class GraphTraverserModule {
 					+ "', because the given callback handler 'traverseHandler' is empty.");
 		}
 
-		// check if traverseId is already in use
-		synchronized (traverseIdTable) {
-			if (traverseIdTable.containsKey(traverseHandler)) {
-				List<String> ids = traverseIdTable.get(traverseHandler);
-				if (ids.contains(traverseId)) {
-					throw new SaltTraverserException("Cannot start traversing graph '" + graph.getId()
-							+ "', because the traverse id '" + traverseId
-							+ "' is already registered for the given callback handler '" + traverseHandler + "'.");
-				} else
-					ids.add(traverseId);
-			} else {
-				// TODO replace this with HashEList
-				List<String> ids = new ArrayList<>();
-				ids.add(traverseId);
-				traverseIdTable.put(traverseHandler, ids);
-			}
-		}
 		Traverser traverser = new Traverser(startNodes, strategy, traverseId, traverseHandler, isCycleSafe, graph);
 		traverser.run();
 	}
@@ -188,172 +160,15 @@ public class GraphTraverserModule {
 				new TopDownDepthFirstTraverser(startNodes, strategy, traverseId, traverseHandler, isCycleSafe, graph)
 						.traverse();
 			} else if (TOP_DOWN_BREADTH_FIRST.equals(strategy)) {
-				// TOP_DOWN_BREADTH_FIRST traversal
-				for (SNode startNode : startNodes) {
-					currentNodePath = new ArrayList<>();
-					if (traverseHandler.checkConstraint(strategy, traverseId, null, startNode, 0l)) {
-						currentNodePath.add(startNode);
-					}
-				}
-				breadthFirst();
+				new TopDownBreadthFirstTraverser(startNodes, strategy, traverseId, traverseHandler, isCycleSafe, graph)
+						.traverse();
 			} else if (BOTTOM_UP_DEPTH_FIRST.equals(strategy)) {
 				new BottomUpDepthFirstTraverser(startNodes, strategy, traverseId, traverseHandler, isCycleSafe, graph)
 						.traverse();
 			} else if (BOTTOM_UP_BREADTH_FIRST.equals(strategy)) {
-				// BOTTOM_UP_BREADTH_FIRST traversal
-				for (SNode startNode : startNodes) {
-					currentNodePath = new ArrayList<SNode>();
-					if (traverseHandler.checkConstraint(strategy, traverseId, null, startNode, 0l)) {
-						currentNodePath.add(startNode);
-					}
-				}
-				breadthFirst();
+				new BottomUpBreadthFirstTraverser(startNodes, strategy, traverseId, traverseHandler, isCycleSafe, graph)
+						.traverse();
 			}
-		}
-
-		/**
-		 * Traverses the graph in breadth first order. Relations are followed
-		 * from source to target. If the {@link Traverser#strategy} is (
-		 * {@link GRAPH_TRAVERSE_TYPE#BOTTOM_UP_BREADTH_FIRST}) relations are
-		 * followed in their opposite direction. <br/>
-		 * During the BFS {@link Traverser#currentNodePath} will only contain
-		 * one node at most. The node most recently visited node. <br/>
-		 * This function isn't able to detect cycles.
-		 * 
-		 * @param rel
-		 *            is the relation, via which the current node (last one in
-		 *            currentPath) was reached
-		 * @param order
-		 *            number of current relation in list of all outgoing
-		 *            relations of the parent node
-		 */
-		private void breadthFirst() {
-			if (isCycleSafe) {
-				throw new SaltException("Not able to detect cycles with breadth first search");
-			}
-			if ((currentNodePath == null) || (currentNodePath.isEmpty())) {
-				throw new SaltParameterException("Cannot traverse node starting at empty start node.");
-			}
-			final boolean isTopDown = strategy != GRAPH_TRAVERSE_TYPE.BOTTOM_UP_BREADTH_FIRST;
-			SNode fromNode = null;
-			SRelation<?, ?> fromRel = null;
-			List<SNode> queuedNodes = new ArrayList<>();
-			List<SNode> queueReachedFrom = new ArrayList<>();
-			List<SRelation<? extends SNode, ? extends SNode>> queueReachedFromRel = new ArrayList<>();
-			List<Integer> queueReachedOrder = new ArrayList<>();
-
-			queuedNodes.addAll(startNodes);
-			for (SNode startNode : startNodes) {
-				queueReachedFrom.add(startNode);
-				queueReachedFromRel.add(null);
-				queueReachedOrder.add(0);
-			}
-			currentNodePath.clear();
-			while (!queuedNodes.isEmpty()) {
-				SNode tNode = queuedNodes.remove(0);
-				fromNode = queueReachedFrom.remove(0);
-				fromRel = queueReachedFromRel.remove(0);
-				Integer order = queueReachedOrder.remove(0);
-
-				List<SRelation<? extends SNode, ? extends SNode>> edgesIn = graph.getInRelations(tNode.getId());
-				List<SRelation<? extends SNode, ? extends SNode>> edgesOut = graph.getOutRelations(tNode.getId());
-				List<SRelation<? extends SNode, ? extends SNode>> edges = null;
-				currentNodePath.add(tNode);
-				traverseHandler.nodeReached(strategy, traverseId, tNode, fromRel, fromNode, order);
-				if (isTopDown) {
-					// Switching in and out nodes list
-					edges = edgesOut;
-				} else {
-					edges = edgesIn;
-					// edgesIn = edgesOut;
-					// edgesOut = edges;
-				}
-				if (edges != null) {
-					// in case of node has childs
-					int orderCount = 0;
-					for (SRelation<? extends SNode, ? extends SNode> e : edges) {
-						SNode n = null;
-						n = (isTopDown) ? e.getTarget() : e.getSource();
-
-						if (traverseHandler.checkConstraint(strategy, traverseId, e, n, order)) {
-							queuedNodes.add(n);
-							queueReachedFrom.add(fromNode);
-							queueReachedFromRel.add(e);
-							queueReachedOrder.add(orderCount);
-						}
-						++orderCount;
-					}
-				}
-				traverseHandler.nodeLeft(strategy, traverseId, tNode, fromRel, fromNode, order);
-				currentNodePath.remove(0);
-			}
-		}
-
-		/**
-		 * Contains the path beginning from root node to current node.
-		 */
-		private List<SNode> currentNodePath = null;
-
-		/**
-		 * A recursive method, which traverses through the graph in top down and
-		 * depth first order. That means, it follows the direction of the edges
-		 * and always expands the node i first, before a node i+1 will be
-		 * expanded. This method is cyclesafe, therefore the variable
-		 * currentNodePath is used.
-		 * 
-		 * @param rel
-		 *            is the edge, via which the current node (last one in
-		 *            currentPath) was reached
-		 * @param order
-		 *            number of current edge in list of all outgoing edges of
-		 *            the parent node
-		 */
-		private void topDownDepthFirstRec(SRelation<? extends SNode, ? extends SNode> rel, long order) {
-			if ((currentNodePath == null) || (currentNodePath.size() == 0)) {
-				throw new SaltParameterException("Cannot traverse node starting at empty start node.");
-			}
-			// current node is last element in currentPath
-			SNode currNode = currentNodePath.get(currentNodePath.size() - 1);
-			SNode parent = null;
-			if (currentNodePath.size() > 1) {
-				// if current path is larger then 1, than a parent node exists
-				parent = currentNodePath.get(currentNodePath.size() - 2);
-			}
-			traverseHandler.nodeReached(strategy, traverseId, currNode, rel, parent, order);
-			// walk through all childs of this node
-			List<SRelation<? extends SNode, ? extends SNode>> childEdges = graph.getOutRelations(currNode.getId());
-			if (childEdges != null) {
-				// in case of node has childs
-				int i = 0;
-				for (SRelation<? extends SNode, ? extends SNode> childRel : childEdges) {
-					SNode childNode = childRel.getTarget();
-
-					if (traverseHandler.checkConstraint(strategy, traverseId, childRel, childNode, order)) {
-						if ((isCycleSafe) && (currentNodePath.contains(childNode))) {
-
-							StringBuffer text = new StringBuffer();
-							for (SNode node : currentNodePath) {
-								text.append(node.getId());
-								text.append(" --> ");
-							}
-							text.append(childNode.getId());
-
-							throw new SaltInvalidModelException("A cycle in graph '" + graph.getId()
-									+ "' has been detected, while traversing with type '" + strategy
-									+ "'. The cycle has been detected when visiting node '" + childNode
-									+ "' while current path was '" + text.toString() + "'.");
-						}
-
-						currentNodePath.add(childNode);
-						topDownDepthFirstRec(childRel, i);
-						currentNodePath.remove(currentNodePath.size() - 1);
-
-						i++;
-					}
-				}
-			}
-
-			traverseHandler.nodeLeft(strategy, traverseId, currNode, rel, parent, order);
 		}
 	}
 }
