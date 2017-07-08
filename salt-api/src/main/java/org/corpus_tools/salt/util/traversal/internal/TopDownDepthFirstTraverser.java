51f79aee75861664a496bab29ba7b99b9cdc339a
florian <f.zipser@gmx.de>
2017-07-08 22:12:44 +0200

    some clean up

--- salt-api/src/main/java/org/corpus_tools/salt/util/traversal/internal/TopDownDepthFirstTraverser.java
+++ salt-api/src/main/java/org/corpus_tools/salt/util/traversal/internal/TopDownDepthFirstTraverser.java
@@ -14,145 +14,6 @@ import org.corpus_tools.salt.util.traversal.BackAndForthTraverseHandler;
 import org.corpus_tools.salt.util.traversal.TraversalStrategy;
 
 public class TopDownDepthFirstTraverser extends Traverser {
-
-	public TopDownDepthFirstTraverser(List<? extends SNode> startNodes, TraversalStrategy strategy, String traverseId,
-			BackAndForthTraverseHandler handler, boolean isCycleSafe, SGraph graph) {
-		super(startNodes, strategy, traverseId, handler, isCycleSafe, graph);
-	}
-
-	@Override
-	public void traverse() {
-		// TOP_DOWN_DEPTH_FIRST traversal
-		for (SNode startNode : startNodes) {
-			if (handler.shouldTraversalGoOn(TraversalLocation.createWithStrategy(strategy)
-					.withCurrentNode(startNode)
-					.withRelationOrder(0)
-					.withId(id)
-					.build())) {
-				Set<SNode> visitedNodes = null;
-				if (isCycleSafe) {
-					// if checking for cycles is enabled, initialize
-					// hashset
-					visitedNodes = new HashSet<>();
-				}
-				Stack<NodeEntry> parentStack = new Stack<>();
-				NodeEntry currentEntry = new NodeEntry(startNode, 0);
-				while ((!parentStack.isEmpty()) || (currentEntry != null)) {
-					if (currentEntry != null) {
-						// way down
-						if (isCycleSafe) {
-							// check if cycle exists
-							if (visitedNodes.contains(currentEntry.node)) {
-								StringBuffer text = new StringBuffer();
-								if (currentNodePath != null) {
-									for (SNode node : currentNodePath) {
-										if (node != null) {
-											text.append(node.getId());
-										} else {
-											text.append("null");
-										}
-										text.append(" --> ");
-									}
-								}
-								text.append(currentEntry.node.getId());
-								throw new SaltInvalidModelException("A cycle in graph '" + graph.getId()
-										+ "' has been detected, while traversing with type '" + strategy
-										+ "'. The cycle has been detected when visiting node '" + currentEntry.node
-										+ "' while current path was '" + text.toString() + "'.");
-							}
-						}
-						NodeEntry peekEntry = null;
-						if (!parentStack.isEmpty())
-							peekEntry = parentStack.peek();
-						parentStack.push(currentEntry);
-						if (isCycleSafe) {
-							// if checking for cycles is enabled,
-							// add node to hashset
-							visitedNodes.add(currentEntry.node);
-						}
-						SNode nextChild = nextChild(currentEntry);
-						if (peekEntry == null) {
-							handler.nodeReachedOnWayForth(TraversalLocation.createWithStrategy(strategy)
-									.withCurrentNode(currentEntry.node)
-									.withRelationOrder(0)
-									.withId(id)
-									.build());
-						} else {
-							handler.nodeReachedOnWayForth(TraversalLocation.createWithStrategy(strategy)
-									.withCurrentNode(currentEntry.node)
-									.withFromRelation(peekEntry.rel)
-									.withFromNode(peekEntry.rel != null ? peekEntry.rel.getSource() : null)
-									.withRelationOrder(peekEntry.order)
-									.withId(id)
-									.build());
-						}
-						if (nextChild != null) {
-							if (handler.shouldTraversalGoOn(TraversalLocation.createWithStrategy(strategy)
-									.withCurrentNode(nextChild)
-									.withFromRelation(currentEntry.rel)
-									.withRelationOrder(currentEntry.order)
-									.withId(id)
-									.build())) {
-								currentEntry = new NodeEntry(nextChild, 0);
-							} else {
-								currentEntry = null;
-							}
-						} else {
-							currentEntry = null;
-						}
-					} else {
-						NodeEntry peekEntry = parentStack.peek();
-						if (peekEntry != null) {
-							SNode nextChild = nextChild(peekEntry);
-							if (nextChild != null) {
-								// way down, another branch was
-								// found
-								if (handler.shouldTraversalGoOn(TraversalLocation.createWithStrategy(strategy)
-										.withCurrentNode(nextChild)
-										.withFromRelation(peekEntry.rel)
-										.withRelationOrder(peekEntry.order)
-										.withId(id)
-										.build())) {
-									currentEntry = new NodeEntry(nextChild, 0);
-								} else {
-									currentEntry = null;
-								}
-							} else {// way up
-								parentStack.pop();
-								if (isCycleSafe) {
-									// if checking for cycles is
-									// enabled, remove node from
-									// hashset
-									visitedNodes.remove(peekEntry.node);
-								}
-								// collect stuff for notification
-								SNode peekNode = peekEntry.node;
-								peekEntry = null;
-								if (!parentStack.isEmpty()) {
-									peekEntry = parentStack.peek();
-								}
-								if (peekEntry == null) {
-									handler.nodeReachedOnWayBack(TraversalLocation.createWithStrategy(strategy)
-											.withCurrentNode(peekNode)
-											.withRelationOrder(0)
-											.withId(id)
-											.build());
-								} else {
-									handler.nodeReachedOnWayBack(TraversalLocation.createWithStrategy(strategy)
-											.withCurrentNode(peekNode)
-											.withFromRelation(peekEntry.rel)
-											.withFromNode(peekEntry.rel != null ? peekEntry.rel.getSource() : null)
-											.withId(id)
-											.build());
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
 	protected static class NodeEntry {
 		private final SNode node;
 		private int order;
@@ -169,7 +30,150 @@ public class TopDownDepthFirstTraverser extends Traverser {
 		}
 	}
 
-	protected SNode nextChild(NodeEntry entry) {
+	private final Set<SNode> visitedNodes = new HashSet<>();
+	private final Stack<NodeEntry> parentStack = new Stack<>();
+	private NodeEntry currentEntry;
+
+	public TopDownDepthFirstTraverser(List<? extends SNode> startNodes, TraversalStrategy strategy, String traverseId,
+			BackAndForthTraverseHandler handler, boolean isCycleSafe, SGraph graph) {
+		super(startNodes, strategy, traverseId, handler, isCycleSafe, graph);
+	}
+
+	@Override
+	public void traverse() {
+		startNodes.stream().forEach(this::traverseNode);
+	}
+
+	public void traverseNode(SNode startNode) {
+		if (!handler.shouldTraversalGoOn(TraversalLocation.createWithStrategy(strategy)
+				.withCurrentNode(startNode)
+				.withRelationOrder(0)
+				.withId(id)
+				.build())) {
+			return;
+		}
+		currentEntry = new NodeEntry(startNode, 0);
+		while ((!parentStack.isEmpty()) || (currentEntry != null)) {
+			if (currentEntry != null) {
+				case1();
+			} else {
+				case2();
+			}
+		}
+	}
+
+	private void case1() {
+		// way down
+		if (isCycleSafe) {
+			// check if cycle exists
+			if (visitedNodes.contains(currentEntry.node)) {
+				StringBuffer text = new StringBuffer();
+				if (currentNodePath != null) {
+					for (SNode node : currentNodePath) {
+						if (node != null) {
+							text.append(node.getId());
+						} else {
+							text.append("null");
+						}
+						text.append(" --> ");
+					}
+				}
+				text.append(currentEntry.node.getId());
+				throw new SaltInvalidModelException(
+						"A cycle in graph '" + graph.getId() + "' has been detected, while traversing with type '"
+								+ strategy + "'. The cycle has been detected when visiting node '" + currentEntry.node
+								+ "' while current path was '" + text.toString() + "'.");
+			}
+		}
+		NodeEntry peekEntry = null;
+		if (!parentStack.isEmpty()) {
+			peekEntry = parentStack.peek();
+		}
+		parentStack.push(currentEntry);
+		if (isCycleSafe) {
+			// if checking for cycles is enabled, add node to hashset
+			visitedNodes.add(currentEntry.node);
+		}
+		SNode nextChild = nextChild(currentEntry);
+		if (peekEntry == null) {
+			handler.nodeReachedOnWayForth(TraversalLocation.createWithStrategy(strategy)
+					.withCurrentNode(currentEntry.node)
+					.withRelationOrder(0)
+					.withId(id)
+					.build());
+		} else {
+			handler.nodeReachedOnWayForth(TraversalLocation.createWithStrategy(strategy)
+					.withCurrentNode(currentEntry.node)
+					.withFromRelation(peekEntry.rel)
+					.withFromNode(peekEntry.rel != null ? peekEntry.rel.getSource() : null)
+					.withRelationOrder(peekEntry.order)
+					.withId(id)
+					.build());
+		}
+		if (nextChild != null) {
+			if (handler.shouldTraversalGoOn(TraversalLocation.createWithStrategy(strategy)
+					.withCurrentNode(nextChild)
+					.withFromRelation(currentEntry.rel)
+					.withRelationOrder(currentEntry.order)
+					.withId(id)
+					.build())) {
+				currentEntry = new NodeEntry(nextChild, 0);
+			} else {
+				currentEntry = null;
+			}
+		} else {
+			currentEntry = null;
+		}
+	}
+
+	private void case2() {
+		NodeEntry peekEntry = parentStack.peek();
+		if (peekEntry != null) {
+			SNode nextChild = nextChild(peekEntry);
+			if (nextChild != null) {
+				// way down, another branch was found
+				if (handler.shouldTraversalGoOn(TraversalLocation.createWithStrategy(strategy)
+						.withCurrentNode(nextChild)
+						.withFromRelation(peekEntry.rel)
+						.withRelationOrder(peekEntry.order)
+						.withId(id)
+						.build())) {
+					currentEntry = new NodeEntry(nextChild, 0);
+				} else {
+					currentEntry = null;
+				}
+			} else {// way up
+				parentStack.pop();
+				if (isCycleSafe) {
+					// if checking for cycles is enabled, remove node from
+					// hashset
+					visitedNodes.remove(peekEntry.node);
+				}
+				// collect stuff for notification
+				SNode peekNode = peekEntry.node;
+				peekEntry = null;
+				if (!parentStack.isEmpty()) {
+					peekEntry = parentStack.peek();
+				}
+				if (peekEntry == null) {
+					handler.nodeReachedOnWayBack(TraversalLocation.createWithStrategy(strategy)
+							.withCurrentNode(peekNode)
+							.withRelationOrder(0)
+							.withId(id)
+							.build());
+				} else {
+					handler.nodeReachedOnWayBack(TraversalLocation.createWithStrategy(strategy)
+							.withCurrentNode(peekNode)
+							.withFromRelation(peekEntry.rel)
+							.withFromNode(peekEntry.rel != null ? peekEntry.rel.getSource() : null)
+							.withId(id)
+							.build());
+				}
+			}
+		}
+	}
+
+	private SNode nextChild(NodeEntry entry) {
 		SNode retVal = null;
 
 		if (entry.order == 0) {
