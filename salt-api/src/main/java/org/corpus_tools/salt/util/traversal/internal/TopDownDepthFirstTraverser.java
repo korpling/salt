4874482f2299517bb66a91682f03057f7c743812
florian <f.zipser@gmx.de>
2017-07-16 23:09:11 +0200

    tried to structure top down depth first traversal

--- salt-api/src/main/java/org/corpus_tools/salt/util/traversal/internal/TopDownDepthFirstTraverser.java
+++ salt-api/src/main/java/org/corpus_tools/salt/util/traversal/internal/TopDownDepthFirstTraverser.java
@@ -5,11 +5,13 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.Stack;
+import java.util.StringJoiner;
 
 import org.corpus_tools.salt.core.SGraph;
 import org.corpus_tools.salt.core.SNode;
 import org.corpus_tools.salt.core.SRelation;
 import org.corpus_tools.salt.exceptions.SaltInvalidModelException;
+import org.corpus_tools.salt.util.SaltUtil;
 import org.corpus_tools.salt.util.traversal.BackAndForthTraverseHandler;
 import org.corpus_tools.salt.util.traversal.SimpleTraverseHandler;
 import org.corpus_tools.salt.util.traversal.TraversalLocation;
@@ -17,13 +19,13 @@ import org.corpus_tools.salt.util.traversal.TraversalStrategy;
 import org.corpus_tools.salt.util.traversal.Traverser;
 
 public class TopDownDepthFirstTraverser extends Traverser {
-	protected static class NodeEntry {
+	protected static class NodeWithOrder {
 		private final SNode node;
 		private int order;
 		private Iterator<SRelation<? extends SNode, ? extends SNode>> iterator;
 		private SRelation<? extends SNode, ? extends SNode> rel;
 
-		public NodeEntry(SNode node, int order) {
+		public NodeWithOrder(SNode node, int order) {
 			this.node = node;
 			this.order = order;
 		}
@@ -34,8 +36,8 @@ public class TopDownDepthFirstTraverser extends Traverser {
 	}
 
 	private final Set<SNode> visitedNodes = new HashSet<>();
-	private final Stack<NodeEntry> parentStack = new Stack<>();
-	private NodeEntry currentEntry;
+	private final Stack<NodeWithOrder> parentStack = new Stack<>();
+	private NodeWithOrder currentEntry;
 
 	public TopDownDepthFirstTraverser(List<? extends SNode> startNodes, TraversalStrategy strategy, String traverseId,
 			BackAndForthTraverseHandler handler, boolean isCycleSafe, SGraph graph) {
@@ -60,64 +62,43 @@ public class TopDownDepthFirstTraverser extends Traverser {
 				.build())) {
 			return;
 		}
-		currentEntry = new NodeEntry(startNode, 0);
+		currentEntry = new NodeWithOrder(startNode, 0);
 		while ((!parentStack.isEmpty()) || (currentEntry != null)) {
 			if (currentEntry != null) {
-				case1();
+				wayForth();
 			} else {
-				case2();
+				wayBack();
 			}
 		}
 	}
 
-	private void case1() {
-		// way down
-		if (isCycleSafe) {
-			// check if cycle exists
-			if (visitedNodes.contains(currentEntry.node)) {
-				StringBuffer text = new StringBuffer();
-				if (currentNodePath != null) {
-					for (SNode node : currentNodePath) {
-						if (node != null) {
-							text.append(node.getId());
-						} else {
-							text.append("null");
-						}
-						text.append(" --> ");
-					}
-				}
-				text.append(currentEntry.node.getId());
-				throw new SaltInvalidModelException(
-						"A cycle in graph '" + graph.getId() + "' has been detected, while traversing with type '"
-								+ strategy + "'. The cycle has been detected when visiting node '" + currentEntry.node
-								+ "' while current path was '" + text.toString() + "'.");
-			}
-		}
-		NodeEntry peekEntry = null;
-		if (!parentStack.isEmpty()) {
-			peekEntry = parentStack.peek();
-		}
+	private void wayForth() {
+		whenGraphContainsCycleThrowExcpetion(currentEntry.node);
+		NodeWithOrder peekEntry = parentStack.isEmpty() ? null : parentStack.peek();
 		parentStack.push(currentEntry);
 		if (isCycleSafe) {
 			// if checking for cycles is enabled, add node to hashset
 			visitedNodes.add(currentEntry.node);
 		}
 		SNode nextChild = nextChild(currentEntry);
+		TraversalLocation location;
 		if (peekEntry == null) {
-			handler.nodeReachedOnWayForth(TraversalLocation.createWithStrategy(strategy)
+			location = TraversalLocation.createWithStrategy(strategy)
 					.withCurrentNode(currentEntry.node)
 					.withRelationOrder(0)
 					.withId(id)
-					.build());
+					.build();
+
 		} else {
-			handler.nodeReachedOnWayForth(TraversalLocation.createWithStrategy(strategy)
+			location = TraversalLocation.createWithStrategy(strategy)
 					.withCurrentNode(currentEntry.node)
 					.withFromRelation(peekEntry.rel)
 					.withFromNode(peekEntry.rel != null ? peekEntry.rel.getSource() : null)
 					.withRelationOrder(peekEntry.order)
 					.withId(id)
-					.build());
+					.build();
 		}
+		handler.nodeReachedOnWayForth(location);
 		if (nextChild != null) {
 			if (handler.shouldTraversalGoOn(TraversalLocation.createWithStrategy(strategy)
 					.withCurrentNode(nextChild)
@@ -125,7 +106,7 @@ public class TopDownDepthFirstTraverser extends Traverser {
 					.withRelationOrder(currentEntry.order)
 					.withId(id)
 					.build())) {
-				currentEntry = new NodeEntry(nextChild, 0);
+				currentEntry = new NodeWithOrder(nextChild, 0);
 			} else {
 				currentEntry = null;
 			}
@@ -134,60 +115,69 @@ public class TopDownDepthFirstTraverser extends Traverser {
 		}
 	}
 
-	private void case2() {
-		NodeEntry peekEntry = parentStack.peek();
-		if (peekEntry != null) {
-			SNode nextChild = nextChild(peekEntry);
-			if (nextChild != null) {
-				// way down, another branch was found
-				if (handler.shouldTraversalGoOn(TraversalLocation.createWithStrategy(strategy)
-						.withCurrentNode(nextChild)
-						.withFromRelation(peekEntry.rel)
-						.withRelationOrder(peekEntry.order)
+	private void whenGraphContainsCycleThrowExcpetion(SNode currentNode) {
+		if (isCycleSafe && visitedNodes.contains(currentNode)) {
+			StringJoiner joiner = new StringJoiner(" -> ");
+			visitedNodes.stream().map(SNode::getId).forEach(joiner::add);
+			joiner.add(currentNode.getId());
+			throw new SaltInvalidModelException(
+					"A cycle in graph '" + graph.getId() + "' has been detected, while traversing with type '"
+							+ strategy + "'. The cycle has been detected when visiting node '" + currentNode
+							+ "' while current path was '" + joiner.toString() + "'.");
+		}
+	}
+
+	private void wayBack() {
+		NodeWithOrder peekEntry = parentStack.peek();
+		if (peekEntry == null) {
+			return;
+		}
+		SNode nextChild = nextChild(peekEntry);
+		if (nextChild != null) {
+			// way down, another branch was found
+			if (handler.shouldTraversalGoOn(TraversalLocation.createWithStrategy(strategy)
+					.withCurrentNode(nextChild)
+					.withFromRelation(peekEntry.rel)
+					.withRelationOrder(peekEntry.order)
+					.withId(id)
+					.build())) {
+				currentEntry = new NodeWithOrder(nextChild, 0);
+			} else {
+				currentEntry = null;
+			}
+		} else {// way up
+			parentStack.pop();
+			if (isCycleSafe) {
+				// if checking for cycles is enabled, remove node from
+				// hashset
+				visitedNodes.remove(peekEntry.node);
+			}
+			// collect stuff for notification
+			SNode peekNode = peekEntry.node;
+			peekEntry = parentStack.isEmpty() ? null : parentStack.peek();
+			if (peekEntry == null) {
+				handler.nodeReachedOnWayBack(TraversalLocation.createWithStrategy(strategy)
+						.withCurrentNode(peekNode)
+						.withRelationOrder(0)
 						.withId(id)
-						.build())) {
-					currentEntry = new NodeEntry(nextChild, 0);
-				} else {
-					currentEntry = null;
-				}
-			} else {// way up
-				parentStack.pop();
-				if (isCycleSafe) {
-					// if checking for cycles is enabled, remove node from
-					// hashset
-					visitedNodes.remove(peekEntry.node);
-				}
-				// collect stuff for notification
-				SNode peekNode = peekEntry.node;
-				peekEntry = null;
-				if (!parentStack.isEmpty()) {
-					peekEntry = parentStack.peek();
-				}
-				if (peekEntry == null) {
-					handler.nodeReachedOnWayBack(TraversalLocation.createWithStrategy(strategy)
-							.withCurrentNode(peekNode)
-							.withRelationOrder(0)
-							.withId(id)
-							.build());
-				} else {
-					handler.nodeReachedOnWayBack(TraversalLocation.createWithStrategy(strategy)
-							.withCurrentNode(peekNode)
-							.withFromRelation(peekEntry.rel)
-							.withFromNode(peekEntry.rel != null ? peekEntry.rel.getSource() : null)
-							.withId(id)
-							.build());
-				}
+						.build());
+			} else {
+				handler.nodeReachedOnWayBack(TraversalLocation.createWithStrategy(strategy)
+						.withCurrentNode(peekNode)
+						.withFromRelation(peekEntry.rel)
+						.withFromNode(peekEntry.rel != null ? peekEntry.rel.getSource() : null)
+						.withId(id)
+						.build());
 			}
 		}
 	}
 
-	private SNode nextChild(NodeEntry entry) {
-		SNode retVal = null;
-
+	private SNode nextChild(NodeWithOrder entry) {
+		SNode nextChild = null;
 		if (entry.order == 0) {
 			// set the iterator
 			List<SRelation<?, ?>> outRels = graph.getOutRelations(entry.node.getId());
-			if ((outRels != null) && (!outRels.isEmpty())) {
+			if (SaltUtil.isNotNullOrEmpty(outRels)) {
 				entry.iterator = outRels.iterator();
 			}
 		}
@@ -195,22 +185,23 @@ public class TopDownDepthFirstTraverser extends Traverser {
 		// if we already have an iterator use it
 		if (entry.iterator != null && entry.iterator.hasNext()) {
 			entry.rel = entry.iterator.next();
-			retVal = entry.rel.getTarget();
+			nextChild = entry.rel.getTarget();
 			entry.order++;
 		}
 
+		// TODO: check whether this can be removed
 		// even if something went wrong with the iterator we should be still
 		// able to execute the code (just with less performance)
 		if (entry.iterator == null) {
 			List<SRelation<? extends SNode, ? extends SNode>> outRels = graph.getOutRelations(entry.node.getId());
-			if ((outRels != null) && (!outRels.isEmpty())) {
+			if (SaltUtil.isNotNullOrEmpty(outRels)) {
 				if (entry.order < outRels.size()) {
 					entry.rel = outRels.get(entry.order);
-					retVal = entry.rel.getTarget();
+					nextChild = entry.rel.getTarget();
 					entry.order++;
 				}
 			}
 		}
-		return (retVal);
+		return (nextChild);
 	}
 }
